300e3a755ba97fac2cdff2a06d47455b491e647a
baobaodev <baohuynh141@gmail.com>
2022-07-04 15:08:42 +0700

    'fixcomflic2'

--- src/pages/ListTypeWorkPage/ListTypeWorkPage.js
+++ src/pages/ListTypeWorkPage/ListTypeWorkPage.js
@@ -1,31 +1,30 @@
-
-import { SiBuddy } from "react-icons/si";
-import React, { useState } from 'react';
 import { Card } from 'antd';
+import { SiBuddy } from 'react-icons/si';
 
-
-import 'antd/dist/antd.css';
-import "./ListTypeWorkPage.scss"
+import 'antd/dist/antd.min.css';
+import './ListTypeWorkPage.scss';
 
 const { Meta } = Card;
 
 export const ListTypeWorkPage = () => {
-
   return (
-    <div className="list-type-work-page">
+    <div className='list-type-work-page'>
       <header>HOC HEADER</header>
 
-      <div className="list-type-work-page-header">
+      <div className='list-type-work-page-header'>
         <h1>Programming And Tech</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, nulla eum. Itaque et vel sequi.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Quas, nulla eum. Itaque et vel sequi.
+        </p>
         <div>
           <SiBuddy />
           <span>How Feverr Wokrs</span>
         </div>
       </div>
 
-      <div className="list-type-work-page-content">
-        <div className="list-type-work-page-content-left">
+      <div className='list-type-work-page-content'>
+        <div className='list-type-work-page-content-left'>
           <h3>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
           <h3>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
           <h3>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
@@ -41,154 +40,223 @@ export const ListTypeWorkPage = () => {
           <h3>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
           <h3>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
         </div>
-        <div className="list-type-work-page-content-right">
-          <div className="list-type-work-page-content-right-item">
+        <div className='list-type-work-page-content-right'>
+          <div className='list-type-work-page-content-right-item'>
             <Card
               hoverable
               style={{
                 width: 240,
               }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              cover={
+                <img
+                  alt='example'
+                  src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                />
+              }
             >
-              <Meta title="Europe Street beat" />
+              <Meta title='Europe Street beat' />
             </Card>
           </div>
-          <div className="list-type-work-page-content-right-item">
+          <div className='list-type-work-page-content-right-item'>
             <Card
               hoverable
               style={{
                 width: 240,
               }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              cover={
+                <img
+                  alt='example'
+                  src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                />
+              }
             >
-              <Meta title="Europe Street beat" />
+              <Meta title='Europe Street beat' />
             </Card>
           </div>
-          <div className="list-type-work-page-content-right-item">
+          <div className='list-type-work-page-content-right-item'>
             <Card
               hoverable
               style={{
                 width: 240,
               }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              cover={
+                <img
+                  alt='example'
+                  src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                />
+              }
             >
-              <Meta title="Europe Street beat" />
+              <Meta title='Europe Street beat' />
             </Card>
           </div>
-          <div className="list-type-work-page-content-right-item">
+          <div className='list-type-work-page-content-right-item'>
             <Card
               hoverable
               style={{
                 width: 240,
               }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              cover={
+                <img
+                  alt='example'
+                  src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                />
+              }
             >
-              <Meta title="Europe Street beat" />
+              <Meta title='Europe Street beat' />
             </Card>
           </div>
-          <div className="list-type-work-page-content-right-item">
+          <div className='list-type-work-page-content-right-item'>
             <Card
               hoverable
               style={{
                 width: 240,
               }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              cover={
+                <img
+                  alt='example'
+                  src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                />
+              }
             >
-              <Meta title="Europe Street beat" />
+              <Meta title='Europe Street beat' />
             </Card>
           </div>
-          <div className="list-type-work-page-content-right-item">
+          <div className='list-type-work-page-content-right-item'>
             <Card
               hoverable
               style={{
                 width: 240,
               }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              cover={
+                <img
+                  alt='example'
+                  src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                />
+              }
             >
-              <Meta title="Europe Street beat" />
+              <Meta title='Europe Street beat' />
             </Card>
           </div>
-          <div className="list-type-work-page-content-right-item">
+          <div className='list-type-work-page-content-right-item'>
             <Card
               hoverable
               style={{
                 width: 240,
               }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              cover={
+                <img
+                  alt='example'
+                  src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                />
+              }
             >
-              <Meta title="Europe Street beat" />
+              <Meta title='Europe Street beat' />
             </Card>
           </div>
-          <div className="list-type-work-page-content-right-item">
+          <div className='list-type-work-page-content-right-item'>
             <Card
               hoverable
               style={{
                 width: 240,
               }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              cover={
+                <img
+                  alt='example'
+                  src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                />
+              }
             >
-              <Meta title="Europe Street beat" />
+              <Meta title='Europe Street beat' />
             </Card>
           </div>
-          <div className="list-type-work-page-content-right-item">
+          <div className='list-type-work-page-content-right-item'>
             <Card
               hoverable
               style={{
                 width: 240,
               }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              cover={
+                <img
+                  alt='example'
+                  src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                />
+              }
             >
-              <Meta title="Europe Street beat" />
+              <Meta title='Europe Street beat' />
             </Card>
           </div>
-          <div className="list-type-work-page-content-right-item">
+          <div className='list-type-work-page-content-right-item'>
             <Card
               hoverable
               style={{
                 width: 240,
               }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              cover={
+                <img
+                  alt='example'
+                  src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                />
+              }
             >
-              <Meta title="Europe Street beat" />
+              <Meta title='Europe Street beat' />
             </Card>
           </div>
-          <div className="list-type-work-page-content-right-item">
+          <div className='list-type-work-page-content-right-item'>
             <Card
               hoverable
               style={{
                 width: 240,
               }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              cover={
+                <img
+                  alt='example'
+                  src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                />
+              }
             >
-              <Meta title="Europe Street beat" />
+              <Meta title='Europe Street beat' />
             </Card>
           </div>
-          <div className="list-type-work-page-content-right-item">
+          <div className='list-type-work-page-content-right-item'>
             <Card
               hoverable
               style={{
                 width: 240,
               }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              cover={
+                <img
+                  alt='example'
+                  src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                />
+              }
             >
-              <Meta title="Europe Street beat" />
+              <Meta title='Europe Street beat' />
             </Card>
           </div>
         </div>
       </div>
-      
-      <div className="list-type-work-page-footer">
-        <div className="list-type-work-page-footer-item">
+
+      <div className='list-type-work-page-footer'>
+        <div className='list-type-work-page-footer-item'>
           <h1>Your safety</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, incidunt.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Omnis, incidunt.
+          </p>
         </div>
-        <div className="list-type-work-page-footer-item">
+        <div className='list-type-work-page-footer-item'>
           <h1>Your safety</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, incidunt.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Omnis, incidunt.
+          </p>
         </div>
-        <div className="list-type-work-page-footer-item">
+        <div className='list-type-work-page-footer-item'>
           <h1>Your safety</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, incidunt.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Omnis, incidunt.
+          </p>
         </div>
       </div>
 
@@ -196,5 +264,3 @@ export const ListTypeWorkPage = () => {
     </div>
   );
 };
-
-
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,5 @@ export * from './ListTypeWorkPage/ListTypeWorkPage';
 export * from './ListWorkPage/ListWorkPage';
 export * from './LoginPage/LoginPage';
 export * from './UserPage/UserPage';
-export * from './WorkDetailPage/WorkDetailPage';
 export * from './Register/Register';
-
+export * from './WorkDetailPage/WorkDetailPage';
