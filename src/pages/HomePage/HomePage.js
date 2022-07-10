32c083cf3febd0e4999213bc77c9913ec3d2e819
NguyenTieuPhung <phung.nt9600@gmail.com>
2022-07-10 22:18:21 +0700

    add files Marketlace

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Category from '../../components/category/Category';
+import Marketplace from '../../components/Marketplace/Marketplace';
 import Selling from '../../components/selling/Selling';
 import Trusted from '../../components/trusted/Trusted';
 import Hero from './../../components/hero/Hero';
@@ -11,6 +12,7 @@ export const HomePage = () => {
       <Trusted />
       <Category />
       <Selling />
+      <Marketplace />
     </div>
   );
 };
