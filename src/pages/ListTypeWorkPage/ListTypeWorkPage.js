98ebdc46c0ce93ceb1f4de9eeaa08a082219b110
Ngô Huy Cường <cuongxuyentu99@gmail.com>
2022-07-28 18:06:41 +0700

    responsive listTypeWorkPage

--- src/pages/ListTypeWorkPage/ListTypeWorkPage.js
+++ src/pages/ListTypeWorkPage/ListTypeWorkPage.js
@@ -68,36 +68,39 @@ export const ListTypeWorkPage = () => {
         </div>
       </div>
       <div className='list-type-work-page-body grid grid-cols-4 gap-4 m-10 container mx-auto'>
-        <div className=''>
+        <div className='tab-sub-type-work'>
           <TabSubTypeWork subTypeJob={subTypeJob} typeJob={typeJob} />
         </div>
-        <div className='col-start-2 col-end-5 grid grid-cols-3 gap-4'>
-          {
-            subTypeJob.map((item) => {
 
-              return (
-                <Card
-                  className='w-full h-full'
-                  hoverable
-                  onClick={() => {
-                    handleClickSubTypeJob(typeJob.id, item._id);
-                  }}
-                  cover={
-                    <img
-                      className='w-full h-full object-cover'
-                      alt='example'
-                      src={item.image || imageAddress}
-                    />
-                  }
-                >
-                  <Meta title={item.name} />
-                </Card>
-              );
-            })
-          }
+        <div className='col-start-2 col-end-5 grid grid-cols-3 gap-4'>
+          <div className='card-sub-type-work'>
+            {
+              subTypeJob.map((item) => {
+
+                return (
+                  <Card
+                    className='w-full h-full'
+                    hoverable
+                    onClick={() => {
+                      handleClickSubTypeJob(typeJob.id, item._id);
+                    }}
+                    cover={
+                      <img
+                        className='w-full h-full object-cover'
+                        alt='example'
+                        src={item.image || imageAddress}
+                      />
+                    }
+                  >
+                    <Meta title={item.name} />
+                  </Card>
+                );
+              })
+            }
+          </div>
         </div>
       </div>
-      <div className='list-type-work-page-footer flex space-x-20 bg-gray-100 p-10'>
+      <div className='list-type-work-page-footer bg-gray-100'>
         <div className='list-type-work-page-footer-item'>
           <h1 className='text-xl'>Your <span className=' font-bold'>Terms</span></h1>
           <p>
